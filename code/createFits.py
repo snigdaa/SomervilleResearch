9e25c3389c32af2008b91091b8112cadb0995e85
Snigdaa <snigdaa96@gmail.com>
2017-06-01 17:26:03 -0400

    created header

--- code/createFits.py
+++ code/createFits.py
@@ -1,1 +1,14 @@
 #create FITs files from numpy arrays containing compiled powderday images in different wavelengths
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from hyperion.model import ModelOutput
+from astropy.cosmology import Planck13
+import astropy.units as u
+import photutils
+from photutils import data_properties, properties_table, EllipticalAperture
+from astropy.io import fits
+
+
